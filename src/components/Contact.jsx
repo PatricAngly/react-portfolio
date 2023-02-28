e71cdae1844b180c84d096c086aea53942c0907f
Patric Angly <padde95@hotmail.se>
2023-02-28 18:14:30 +0100

    change h-full breakpoint

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-slate-400 p-4 text-white"
+      className="w-full md:h-screen bg-gradient-to-b from-black to-slate-400 p-4 text-white"
     >
       <div className="flex flex-col p-4 max-w-screen-lg mx-auto pt-32">
         <div className="pb-8">
