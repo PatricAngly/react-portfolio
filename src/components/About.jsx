8060b7c01da8ed830500a404ca8f6369cdf98762
Patric Angly <padde95@hotmail.se>
2023-02-28 19:14:55 +0100

    change p-tagg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,9 +27,9 @@ const About = () => {
           The more I learned about web development, the more I realized the
           importance of creating user-friendly and accessible websites. I began
           to focus on front-end development and learned about HTML, CSS, and
-          JavaScript. The more I worked with it, the more I became fascinated
-          with the opportunity to improve the user experience and make websites
-          more intuitive and accessible for everyone.
+          JavaScript. The more I became fascinated with the opportunity to
+          improve the user experience and make websites more intuitive and
+          accessible for everyone.
         </p>
 
         <br />
