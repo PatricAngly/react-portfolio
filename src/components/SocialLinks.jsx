3ce410d0d98acc67aa5a0bd3dbd2fc72b3bb81a6
Patric Angly <padde95@hotmail.se>
2024-02-27 14:22:03 +0100

    resume pdf update

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Patric_Angly_eng.pdf",
+      href: "/PatricAngly2024eng.pdf",
       style: "rounded-br-md",
       download: true,
     },
